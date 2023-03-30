25e3657a05c06119980b6fa58e121d67ac07cba5
chrislemus <icrislemus@gmail.com>
2023-03-30 18:02:42 -0400

    update button styling

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,10 +2,17 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  template: ` <p>{{ value }}</p> `,
-  styles: [],
+  template: `
+    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton hero status="success" *ngIf="value == 'X'">
+      {{ value }}
+    </button>
+    <button nbButton hero status="info" *ngIf="value == 'O'">
+      {{ value }}
+    </button>
+  `,
+  styles: ['button {height: 100%; width: 100%}'],
 })
 export class SquareComponent {
   @Input() value: 'X' | 'O' = 'O';
-  rando = Math.random();
 }
