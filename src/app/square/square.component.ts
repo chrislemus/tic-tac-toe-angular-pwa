d4f7d75ebfdb1b954da5a95606d9ed5f117eea5c
chrislemus <icrislemus@gmail.com>
2023-03-30 17:41:39 -0400

    add sqaure component and template

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  template: ` <p>square works! {{ value }}</p> `,
+  template: ` <p>{{ value }}</p> `,
   styles: [],
 })
 export class SquareComponent {
