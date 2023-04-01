978d0d33b3e972619b76a5298dc813781c436899
chrislemus <icrislemus@gmail.com>
2023-03-31 22:59:01 -0400

    refactor square.component

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,15 +3,17 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <button nbButton *ngIf="!value">{{ value }}</button>
-    <button nbButton hero status="success" *ngIf="value == 'X'">
-      {{ value }}
-    </button>
-    <button nbButton hero status="info" *ngIf="value == 'O'">
+    <button nbButton *ngIf="!value"></button>
+    <button
+      *ngIf="!!value"
+      nbButton
+      hero
+      [status]="value == 'X' ? 'success' : 'info'"
+    >
       {{ value }}
     </button>
   `,
-  styles: ['button {height: 100%; width: 100%}'],
+  styles: ['button {height: 100%; width: 100%; }'],
 })
 export class SquareComponent {
   @Input() value: 'X' | 'O' = 'O';
